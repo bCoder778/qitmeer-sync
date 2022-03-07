8c65083a3fc2e6e20c5f0ab90cc0b7ba9c7c0a28
bCoder778 <87968445+QDanlSc@users.noreply.github.com>
2022-03-07 09:37:31 +0800

    Update sync.go

--- sync/sync.go
+++ sync/sync.go
@@ -54,18 +54,18 @@ func (qs *QitmeerSync) Stop() {
 func (qs *QitmeerSync) Run() {
 	qs.wg.Add(1)
 	go qs.syncBlock()
+	/*
+		qs.wg.Add(1)
+		go qs.syncTxPool()
 
-	qs.wg.Add(1)
-	go qs.syncTxPool()
+		qs.wg.Add(1)
+		go qs.updateUnconfirmedBlock()
 
-	qs.wg.Add(1)
-	go qs.updateUnconfirmedBlock()
+		qs.wg.Add(1)
+		go qs.updateUnconfirmedTransaction()
 
-	qs.wg.Add(1)
-	go qs.updateUnconfirmedTransaction()
-
-	qs.wg.Add(1)
-	go qs.updateCoins()
+		qs.wg.Add(1)
+		go qs.updateCoins()*/
 
 	qs.wg.Wait()
 	if err := qs.storage.Close(); err != nil {
@@ -256,11 +256,11 @@ func (qs *QitmeerSync) requestBlock(group *sync.WaitGroup) {
 						}
 					}
 				}
-				lastOrder = block.Order
 				log.Debugf("Request block id %d failed! %s", start, err.Error())
 				time.Sleep(time.Second * waitBlockTime)
 				continue
 			}
+			lastOrder = block.Order
 			start++
 			qs.blockCh <- block
 		}
