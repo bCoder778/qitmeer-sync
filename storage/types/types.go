2629cdae6c34b6d50e93f2167e7847acc7280bd2
bCoder778 <87968445+QDanlSc@users.noreply.github.com>
2022-03-31 12:09:10 +0800

    Update types.go

--- storage/types/types.go
+++ storage/types/types.go
@@ -7,30 +7,30 @@ import (
 type Block struct {
 	Id            uint64         `xorm:"bigint pk" json:"id"`
 	Hash          string         `xorm:"varchar(64) index" json:"hash"`
-	Txvalid       bool           `xorm:"bool" json:"txvalid"`
+	Txvalid       bool           `xorm:"bool index" json:"txvalid"`
 	Confirmations uint64         `xorm:"bigint index" json:"confirmation"`
 	Version       uint32         `xorm:"int" json:"version"`
 	Weight        uint64         `xorm:"bigint" json:"weight"`
-	Height        uint64         `xorm:"bigint" json:"height"`
+	Height        uint64         `xorm:"bigint index" json:"height"`
 	TxRoot        string         `xorm:"varchar(64)" json:"txroot"`
 	Order         uint64         `xorm:"bigint index" json:"order"`
 	Transactions  int            `xorm:"int" json:"transactions"`
 	StateRoot     string         `xorm:"varchar(64)" json:"stateroot"`
 	Bits          string         `xorm:"varchar(64)" json:"bits"`
-	Timestamp     int64          `xorm:"bigint" json:"timestamp"`
+	Timestamp     int64          `xorm:"bigint index" json:"timestamp"`
 	ParentRoot    string         `xorm:"varchar(64)" json:"parantroot"`
 	Parents       []string       `xorm:"json" json:"parents"`
 	Children      []string       `xorm:"json" json:"children"`
 	Difficulty    uint64         `xorm:"bigint" json:"difficulty"`
-	PowName       string         `xorm:"varchar(20)" json:"powname"`
+	PowName       string         `xorm:"varchar(20) index" json:"powname"`
 	PowType       int            `xorm:"int" json:"powtype"`
 	Nonce         string         `xorm:"varchar(32)" json:"nonce"`
 	EdgeBits      int            `xorm:"int" json:"edgebits"`
 	CircleNonces  string         `xorm:"Text" json:"circlenonces"`
-	Address       string         `xorm:"varchar(65)" json:"address"`
+	Address       string         `xorm:"varchar(65) index" json:"address"`
 	Amount        uint64         `xorm:"bigint" json:"amount"`
 	PeerId        string         `xorm:"varchar(64)" json:"peerid"`
-	Color         stat.Color     `xorm:"int" json:"color"`
+	Color         stat.Color     `xorm:"int index" json:"color"`
 	Stat          stat.BlockStat `xorm:"int index" json:"stat"`
 }
 
@@ -43,16 +43,16 @@ type Transaction struct {
 	Id            uint64      `xorm:"bigint autoincr pk" json:"id"`
 	TxId          string      `xorm:"varchar(64) index" json:"txid"`
 	BlockHash     string      `xorm:"varchar(64) index" json:"blockhash"`
-	BlockOrder    uint64      `xorm:"bigint" json:"blockorder"`
+	BlockOrder    uint64      `xorm:"bigint index" json:"blockorder"`
 	TxHash        string      `xorm:"varchar(64)" json:"txhash"`
 	Size          int         `xorm:"int" json:"size"`
 	Version       uint32      `xorm:"int" json:"version"`
 	Locktime      uint64      `xorm:"bigint" json:"locktime"`
-	Timestamp     int64       `xorm:"bigint" json:"timestamp"`
+	Timestamp     int64       `xorm:"bigint index" json:"timestamp"`
 	Expire        uint64      `xorm:"bigint" json:"expire"`
-	Confirmations uint64      `xorm:"bigint" json:"confirmations"`
-	Txsvaild      bool        `xorm:"bool" json:"txsvaild"`
-	IsCoinbase    bool        `xorm:"bool" json:"iscoinbase"`
+	Confirmations uint64      `xorm:"bigint index" json:"confirmations"`
+	Txsvaild      bool        `xorm:"bool index" json:"txsvaild"`
+	IsCoinbase    bool        `xorm:"bool index" json:"iscoinbase"`
 	VinAmount     uint64      `xorm:"bigint" json:"vinamount"`
 	VoutAmount    uint64      `xorm:"bigint" json:"voutamount"`
 	VinAddress    string      `xorm:"varchar(65)" json:"vinaddress"`
@@ -61,23 +61,23 @@ type Transaction struct {
 	Vins          int         `xorm:"int" json:"vin"`
 	Vouts         int         `xorm:"int" json:"vout"`
 	Fees          uint64      `xorm:"bigint" json:"fees"`
-	Duplicate     bool        `xorm:"bool" json:"duplicate"`
+	Duplicate     bool        `xorm:"bool index" json:"duplicate"`
 	Stat          stat.TxStat `xorm:"int index" json:"stat"`
 }
 
 type Transfer struct {
 	Id            uint64      `xorm:"bigint autoincr pk" json:"id"`
 	TxId          string      `xorm:"varchar(64) index" json:"txid"`
-	BlockHash     string      `xorm:"varchar(64)" json:"block_hash"`
+	BlockHash     string      `xorm:"varchar(64) index" json:"block_hash"`
 	Address       string      `xorm:"varchar(65) index" json:"address"`
 	CoinId        string      `xorm:"varchar(30) index" json:"conid"`
-	Confirmations uint64      `xorm:"bigint" json:"confirmations"`
-	Txsvaild      bool        `xorm:"bool" json:"txsvaild"`
+	Confirmations uint64      `xorm:"bigint index" json:"confirmations"`
+	Txsvaild      bool        `xorm:"bool index" json:"txsvaild"`
 	IsCoinbase    bool        `xorm:"bool index" json:"iscoinbase"`
-	IsBlue        bool        `xorm:"bool" json:"isblue"`
+	IsBlue        bool        `xorm:"bool index" json:"isblue"`
 	Change        int64       `xorm:"bigint index" json:"change"`
 	Fees          uint64      `xorm:"bigint" json:"fees"`
-	Timestamp     int64       `xorm:"bigint" json:"timestamp"`
+	Timestamp     int64       `xorm:"bigint index" json:"timestamp"`
 	Stat          stat.TxStat `xorm:"int index" json:"stat"`
 	Duplicate     bool
 }
@@ -86,17 +86,17 @@ type Vin struct {
 	Id            uint64      `xorm:"bigint autoincr pk" json:"id"`
 	TxId          string      `xorm:"varchar(64) index(queryvin)" json:"txid"`
 	Number        int         `xorm:"int index" json:"number"`
-	Order         uint64      `xorm:"bigint" json:"order"`
-	Timestamp     int64       `xorm:"bigint" json:"timestamp"`
+	Order         uint64      `xorm:"bigint index" json:"order"`
+	Timestamp     int64       `xorm:"bigint index" json:"timestamp"`
 	Address       string      `xorm:"varchar(65) index" json:"address"`
 	CoinId        string      `xorm:"varchar(5) index" json:"conid"`
 	Amount        uint64      `xorm:"bigint" json:"amount"`
 	SpentedTx     string      `xorm:"varchar(64)" json:"spentedtx"`
 	Vout          int         `xorm:"int index(queryvin)" json:"vout"`
-	Confirmations uint64      `xorm:"bigint" json:"confirmations"`
+	Confirmations uint64      `xorm:"bigint index" json:"confirmations"`
 	Sequence      uint64      `xorm:"bigint" json:"sequence"`
 	ScriptSig     *ScriptSig  `xorm:"json" json:"scriptsig"`
-	Stat          stat.TxStat `xorm:"int" json:"stat"`
+	Stat          stat.TxStat `xorm:"int index" json:"stat"`
 	Duplicate     bool
 }
 
@@ -104,19 +104,19 @@ type Vout struct {
 	Id            uint64        `xorm:"bigint autoincr pk" json:"id"`
 	TxId          string        `xorm:"varchar(64) index" json:"txid"`
 	Number        int           `xorm:"int index" json:"number"`
-	Order         uint64        `xorm:"bigint" json:"order"`
-	Height        uint64        `xorm:"bigint" json:"height"`
-	Timestamp     int64         `xorm:"bigint" json:"timestamp"`
+	Order         uint64        `xorm:"bigint index" json:"order"`
+	Height        uint64        `xorm:"bigint index" json:"height"`
+	Timestamp     int64         `xorm:"bigint index" json:"timestamp"`
 	Address       string        `xorm:"varchar(65) index" json:"address"`
 	PKAddress     string        `xorm:"varchar(65) index pk_address" json:"pkAddress"`
 	EVMAddress    string        `xorm:"varchar(65) index evm_address" json:"evmAddress"`
-	Amount        uint64        `xorm:"bigint" json:"amount"`
+	Amount        uint64        `xorm:"bigint index" json:"amount"`
 	CoinId        string        `xorm:"varchar(30) index" json:"coinid"`
 	IsCoinbase    bool          `xorm:"bool index" json:"iscoinbase"`
 	IsBlue        bool          `xorm:"bool index" json:"isblue"`
 	ScriptPubKey  *ScriptPubKey `xorm:"json" json:"scriptpubkey"`
 	SpentTx       string        `xorm:"varchar(64) index" json:"spenttx"`
-	Confirmations uint64        `xorm:"bigint" json:"confirmations"`
+	Confirmations uint64        `xorm:"bigint index" json:"confirmations"`
 	Stat          stat.TxStat   `xorm:"int index" json:"stat"`
 	Lock          uint64        `xorm:"bigint index" json:"lock"`
 	Duplicate     bool
